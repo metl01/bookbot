df18336333a0f31d15397b535fd872698af39da1
Metl01 <brewster.noah1@gmail.com>
2026-05-24 00:47:20 -0400

    add ability to choose file path, finished sort function, added report generation

--- main.py
+++ main.py
@@ -1,19 +1,25 @@
 from stats import word_count
 from stats import letter_count
 from stats import sorted_count
+import sys
 
 def main():
-    book_path = "./books/frankenstein.txt"
+    print("Usage: python3 main.py <path_to_book>")
+    book_path = sys.argv[1]
     text = get_book_text(book_path)
     total_count = f"Found {word_count(text)} total words"
     total_letters = letter_count(text)
     sorted_list = sorted_count(total_letters)
+    print("============ BOOKBOT ============")
+    print(f"Analyzing book found at {book_path}...")
+    print("----------- Word Count ----------")
     print(total_count)
-    #print(total_letters)
-    print(sorted_list)
-
-
-
+    print("--------- Character Count -------")
+    for item in sorted_list:
+        if not item["char"].isalpha():
+            continue
+        print(f"{item["char"]}: {item["num"]}")
+    print("============= END ===============")
 
 def get_book_text(filepath: str) -> str:
     with open(filepath) as f:
